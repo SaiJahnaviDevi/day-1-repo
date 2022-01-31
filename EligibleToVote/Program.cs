8bf0659854b6bf49b01afb382fff89ea46986d44
SaiJahnaviDevi <saijahnavidevichintala@gmail.com>
2022-01-31 19:52:30 +0530

    first commit

---
+++ EligibleToVote/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EligibleToVote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Eligible to vote");
+            }
+            else
+                Console.WriteLine("Not eligible to vote");
+        }
+    }
+}
