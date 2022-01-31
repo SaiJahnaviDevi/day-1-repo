60236c8518bcd96aa65104cab2c911b96fcbf178
SaiJahnaviDevi <saijahnavidevichintala@gmail.com>
2022-01-31 18:37:14 +0530

    first commit

---
+++ SampleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SampleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+                Console.WriteLine(n + " is even number");
+            else
+                Console.WriteLine(n + " is odd number");
+        }
+    }
+}
---
+++ Simple/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 18)
+            {
+                Console.WriteLine("age is above 18");
+            }
+            else
+                Console.WriteLine("age is below 18");
+        }
+    }
+}
