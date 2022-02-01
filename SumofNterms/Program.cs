3b430d0157c74df73aac6cec498678e84d42d5c3
SaiJahnaviDevi <saijahnavidevichintala@gmail.com>
2022-02-01 10:19:50 +0530

    day2

---
+++ SumofNterms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumofNterms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("enter the number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("First {0} natural numbers are:", n);
+            for(int i = 1; i <= n; i++)
+            {
+                Console.Write(i+"\t");
+                sum = sum + i;
+            }
+            Console.WriteLine("\nsum of {0} natural numbers= {1}",n,sum);
+        }
+    }
+}
