0cda6ecf5a09eb4ac417aab5f075c205c813622d
SaiJahnaviDevi <saijahnavidevichintala@gmail.com>
2022-02-01 20:40:21 +0530

    transpose,2nd largest

---
+++ ConsoleApp2/SecondLargest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class SencondLargest 
+    {
+        static void Main1(string[] args)
+        {
+            int i,j=0,lar,lar2;
+            int[] a = new int[100];
+            Console.WriteLine("Enter size of array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter array elements:");
+            for (i = 0; i < n; i++)
+            {
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            lar = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (lar < a[i])
+                {
+                    lar = a[i];
+                    j = i;
+                }
+            }
+            lar2 = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (i == j)
+                {
+                    i++;
+                    i--;
+                }
+                else
+                {
+                    if (lar2 < a[i])
+                        lar2 = a[i];
+                }
+            }
+            Console.WriteLine("second largest element is : {0}", lar2);
+        }
+    }
+}
---
+++ ConsoleApp2/Transpose.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Transpose
+    {
+        static void Main(String[] args)
+        {
+            int i, j, m, n;
+            int[,] arr1 = new int[30, 30];
+            int[,] arr2 = new int[30, 30];
+            Console.Write("Enter no.of rows = ");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter no.of columns = ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Set elements in the matrix...\n");
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write("[{0}],[{1}] : ", i, j);
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.Write("\nMatrix before Transpose:\n");
+            for (i = 0; i < m; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < n; j++)
+                    Console.Write("{0}\t", arr1[i, j]);
+            }
+            for (i = 0; i < m; i++)
+            {
+                for (j = 0; j < n; j++)
+                {
+
+                    arr2[j, i] = arr1[i, j];
+                }
+            }
+            Console.Write("\n\nMatrix after Transpose:\n");
+            for (i = 0; i < m; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < n; j++)
+                {
+                    Console.Write("{0}\t", arr2[i, j]);
+                }
+            }
+            Console.Write("\n\n");
+        }
+    }
+}
