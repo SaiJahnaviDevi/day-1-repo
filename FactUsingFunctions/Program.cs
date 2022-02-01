2b6d1547f89d98a145d80c94c87f7dce66bbb60b
SaiJahnaviDevi <saijahnavidevichintala@gmail.com>
2022-02-01 11:13:29 +0530

    day2

---
+++ FactUsingFunctions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactUsingFunctions
+{
+    class Program
+    {
+        public int Fact(int a)
+        {
+            int fact = 1;
+            for(int i = 1; i <= a; i++)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Program program = new Program();
+            Console.WriteLine("factorial of {0} is {1}",n,program.Fact(n));
+        }
+    }
+}
